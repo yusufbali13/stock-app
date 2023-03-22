5b62266aa8df71627020e76e6cb3e8bdcd5e9e66
Yusuf <baliyusuf675@gmail.com>
2023-03-22 21:38:17 +0300

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,13 +6,14 @@ import Typography from "@mui/material/Typography";
 import LockIcon from "@mui/icons-material/Lock";
 import image from "../assets/result.svg";
 import { Link, useNavigate } from "react-router-dom";
-
 import { useSelector } from "react-redux";
+import { Formik } from "formik";
 
 const Login = () => {
   const navigate = useNavigate();
   const { currentUser, error } = useSelector((state) => state?.auth);
 
+  const loginScheme = {};
   return (
     <Container maxWidth="lg">
       <Grid
@@ -49,6 +50,19 @@ const Login = () => {
           >
             Login
           </Typography>
+
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            validationSchema={loginScheme}
+            onSubmit={(values, actions) => {
+              //TODO login(values) POST isteği
+              //TODO navigate
+              actions.resetForm();
+              actions.setSubmitting(false);
+            }}
+          >
+            {({ values, handleChange, handleBlur, errors }) => {}}
+          </Formik>
 
           <Box sx={{ textAlign: "center", mt: 2 }}>
             <Link to="/register">Do you have not an account?</Link>
