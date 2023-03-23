2e3af2b60a19f4b27144df11eb77e26b83a5626b
Yusuf <baliyusuf675@gmail.com>
2023-03-23 23:45:37 +0300

    store

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
+import { persistStore, persistReducer } from "redux-persist";
 
 const store = configureStore({
   reducer: {
