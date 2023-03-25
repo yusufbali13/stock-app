c28f48b966e6e23ed29e0f81fa5d017e9e9de092
Yusuf <baliyusuf675@gmail.com>
2023-03-25 14:38:04 +0300

    url

--- src/hooks/useAuthCall.jsx
+++ src/hooks/useAuthCall.jsx
@@ -15,7 +15,7 @@ const useAuthCall = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const BASE_URL = "https://10001.fullstack.clarusway.com/";
+  const BASE_URL = "https://12147.fullstack.clarusway.com/";
 
   const login = async (userInfo) => {
     dispatch(fetchStart());
