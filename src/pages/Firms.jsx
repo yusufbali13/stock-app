e46571d2c4e0d5a41f18c08642f076f91f6fbd51
Yusuf <baliyusuf675@gmail.com>
2023-03-25 12:01:17 +0300

    firms

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,5 +1,34 @@
+import { Button } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchStart } from "../features/authSlice";
+
 const Firms = () => {
-  return <div>Firms</div>;
+  const { token } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  const getFirms = async () => {
+    const BASE_URL = "https://12147.fullstack.clarusway.com/";
+
+    dispatc(fetchStart());
+    const { data } = await axios(`${BASE_URL}stock/firms`, {
+      headers: { Authorization: `Token ${token}` },
+    });
+  };
+
+  useEffect(() => {
+    getFirms();
+  }, []);
+  return (
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Firm
+      </Typography>
+
+      <Button variant="contained">New Film</Button>
+    </div>
+  );
 };
 
 export default Firms;
