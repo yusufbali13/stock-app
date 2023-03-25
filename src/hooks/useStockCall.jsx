f5c65c8bfa18b67215ba07baa695350e16e613ba
Yusuf <baliyusuf675@gmail.com>
2023-03-25 16:43:54 +0300

    useStock

--- src/hooks/useStockCall.jsx
+++ src/hooks/useStockCall.jsx
@@ -1,5 +1,5 @@
 // import axios from "axios"
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { fetchFail, getSuccess, fetchStart } from "../features/stockSlice";
 import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 import useAxios from "./useAxios";
