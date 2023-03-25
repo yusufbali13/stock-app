063208f56717e9165da28be2779ee9ade1ab15a0
Yusuf <baliyusuf675@gmail.com>
2023-03-25 14:28:12 +0300

    global

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,38 +1,56 @@
 import { Button, Grid } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { useEffect } from "react";
-import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchStart } from "../features/authSlice";
+import { useSelector } from "react-redux";
+import FirmCard from "../components/FirmCard";
+import useStockCall from "../hooks/useStockCall";
+import { flex } from "../styles/globalStyle";
+
+// import axios from "axios"
+// import { useDispatch, useSelector } from "react-redux"
+// import { fetchFail, getSuccess, fetchStart } from "../features/stockSlice"
 
 const Firms = () => {
-  const { token } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
+  // const { token } = useSelector((state) => state.auth)
+  // const dispatch = useDispatch()
 
-  const getFirms = async () => {
-    const BASE_URL = "https://12147.fullstack.clarusway.com/";
+  // const getFirms = async () => {
+  //   const BASE_URL = "https://10001.fullstack.clarusway.com/"
+  //   dispatch(fetchStart())
+  //   const url = "firms"
+  //   try {
+  //     const { data } = await axios(`${BASE_URL}stock/firms/`, {
+  //       headers: { Authorization: `Token ${token}` },
+  //     })
+  //     dispatch(getSuccess({ data, url }))
+  //   } catch (error) {
+  //     console.log(error)
+  //     dispatch(fetchFail())
+  //   }
+  // }
 
-    dispatc(fetchStart());
-    const { data } = await axios(`${BASE_URL}stock/firms`, {
-      headers: { Authorization: `Token ${token}` },
-    });
-  };
+  const { getStockData } = useStockCall();
+  const { firms } = useSelector((state) => state.stock);
 
   useEffect(() => {
-    getFirms();
+    // getFirms()
+    getStockData("firms");
   }, []);
+
+  console.log(firms);
+
   return (
     <div>
       <Typography variant="h4" color="error" mb={3}>
         Firm
       </Typography>
 
-      <Button variant="contained">New Film</Button>
+      <Button variant="contained">New Firm</Button>
 
-      <Grid container>
-        {Firms?.map(() => (
+      <Grid container sx={flex}>
+        {firms?.map((firm) => (
           <Grid item key={firm.id}>
-            <FirmCard />
+            <FirmCard firm={firm} />
           </Grid>
         ))}
       </Grid>
