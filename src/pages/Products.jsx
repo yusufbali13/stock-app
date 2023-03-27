1c44fbc01127822be1694f24db3df38b0b63bc19
Yusuf <baliyusuf675@gmail.com>
2023-03-28 00:12:11 +0300

    products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,60 @@
+import { Button, Grid } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import ProductCard from "../components/ProductCard";
+import ProductModal from "../components/modals/ProductModal";
+import useStockCall from "../hooks/useStockCall";
+import { flex } from "../styles/globalStyle";
+
 const Products = () => {
-  return <div>Products</div>;
+  const { getStockData } = useStockCall();
+  const { products } = useSelector((state) => state.stock);
+  const [open, setOpen] = useState(false);
+
+  const [info, setInfo] = useState({
+    name: "",
+    phone: "",
+    address: "",
+    image: "",
+  });
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  useEffect(() => {
+    getStockData("products");
+  }, []);
+
+  return (
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Products
+      </Typography>
+
+      <Button variant="contained" onClick={handleOpen}>
+        New Product
+      </Button>
+
+      <ProductModal
+        open={open}
+        handleClose={handleClose}
+        info={info}
+        setInfo={setInfo}
+      />
+      <Grid container sx={flex}>
+        {products?.map((product) => (
+          <Grid item key={product.id}>
+            <ProductCard
+              product={product}
+              setOpen={setOpen}
+              setInfo={setInfo}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 };
 
 export default Products;
