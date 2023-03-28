70f42576de57a07124d79b65ad73c7b4497525dc
Yusuf <baliyusuf675@gmail.com>
2023-03-28 17:12:07 +0300

    products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,19 @@
-import { Button, Grid } from "@mui/material";
+import { Button } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import ProductCard from "../components/ProductCard";
+
 import ProductModal from "../components/modals/ProductModal";
 import useStockCall from "../hooks/useStockCall";
-import { flex } from "../styles/globalStyle";
+
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 const Products = () => {
   const { getStockData } = useStockCall();
@@ -42,17 +50,38 @@ const Products = () => {
         info={info}
         setInfo={setInfo}
       />
-      <Grid container sx={flex}>
-        {products?.map((product) => (
-          <Grid item key={product.id}>
-            <ProductCard
-              product={product}
-              setOpen={setOpen}
-              setInfo={setInfo}
-            />
-          </Grid>
-        ))}
-      </Grid>
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="right">#</TableCell>
+              <TableCell align="right">Category</TableCell>
+              <TableCell align="right">Brand</TableCell>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Stock</TableCell>
+              <TableCell align="right">Operation</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products?.map((pro, index) => (
+              <TableRow
+                key={pro.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row" align="right">
+                  {index + 1}
+                </TableCell>
+                <TableCell align="right">{pro.category}</TableCell>
+                <TableCell align="right">{pro.brand}</TableCell>
+                <TableCell align="right">{pro.name}</TableCell>
+                <TableCell align="right">{pro.stock}</TableCell>
+                <TableCell align="right"></TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
