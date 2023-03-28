da168b0e7edff4486c980d7802a3833cc6c85210
Yusuf <baliyusuf675@gmail.com>
2023-03-28 14:19:36 +0300

    globalstyle

--- src/styles/globalStyle.jsx
+++ src/styles/globalStyle.jsx
@@ -9,7 +9,6 @@ export const btnStyle = {
   cursor: "pointer",
   "&:hover": { color: "red" },
 };
-
 export const modalStyle = {
   position: "absolute",
   top: "50%",
@@ -20,4 +19,16 @@ export const modalStyle = {
   border: "2px solid #000",
   boxShadow: 24,
   p: 4,
+};
+export const flexCenter = {
+  display: "flex",
+  flexDirection: { xs: "column", sm: "row" },
+  justifyContent: "center",
+  alignItems: "center",
+  gap: 2,
+};
+export const flexColumn = {
+  display: "flex",
+  flexDirection: "column",
+  gap: 2,
 };
