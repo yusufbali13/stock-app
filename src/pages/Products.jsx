cae950ae01f36528ecf141fdd3f9390c574baf91
Yusuf <baliyusuf675@gmail.com>
2023-03-29 22:50:47 +0300

    delete

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -8,7 +8,9 @@ import useStockCall from "../hooks/useStockCall";
 
 import * as React from "react";
 import Box from "@mui/material/Box";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
+import { DeleteForever } from "@mui/icons-material";
+import { btnStyle } from "../styles/globalStyle";
 
 const Products = () => {
   const { getStockData } = useStockCall();
@@ -77,19 +79,15 @@ const Products = () => {
       align: "center",
       minWidth: 50,
       flex: 1,
+      renderCell: (params) => (
+        <GridActionsCellItem
+          icon={<DeleteForever />}
+          label="Delete"
+          sx={btnStyle}
+          onClick={() => deleteStockData("products", id)}
+        />
+      ),
     },
-  ];
-
-  const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
   ];
 
   useEffect(() => {
