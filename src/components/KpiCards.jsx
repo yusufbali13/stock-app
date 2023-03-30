f461ad4b500f79baf5bbf0d6f24a2af8d910cafb
Yusuf <baliyusuf675@gmail.com>
2023-03-30 22:19:26 +0300

    styled

--- src/components/KpiCards.jsx
+++ src/components/KpiCards.jsx
@@ -1,9 +1,13 @@
-import { Grid } from "@mui/material";
+import { Avatar, Grid, Paper, Typography } from "@mui/material";
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PaymentsIcon from "@mui/icons-material/Payments";
+import { Box } from "@mui/system";
 
 const KpiCards = () => {
+  const iconSize = {
+    ".MuiSvgIcon-root": { fontSize: "2" },
+  };
   const data = [
     {
       id: 1,
@@ -25,16 +29,36 @@ const KpiCards = () => {
       id: 3,
       title: "purchases",
       value: "$10000",
-      icon: <ShoppingCartIcon />,
+      icon: <ShoppingCartIcon sx={iconSize} />,
       color: amber[600],
       bgColor: amber[100],
     },
   ];
 
   return (
-    <Grid container>
-      {data.map()}
-      <Grid item></Grid>
+    <Grid container justifyContent={"center"} spacing={3}>
+      {data.map((item) => (
+        <Grid item sm={12} md={6} lg={4}>
+          <Paper sx={{ p: 2 }}>
+            <Box sx={{ display: "flex", alignItems: "center", gap: 3 }}>
+              <Avatar
+                sx={{
+                  backgroundColor: item.bgColor,
+                  color: item.color,
+                  width: 60,
+                  height,
+                }}
+              >
+                {item.icon}
+              </Avatar>
+              <Box>
+                <Typography variant="button">{item.title}</Typography>
+                <Typography variant="h4">{item.value}</Typography>
+              </Box>
+            </Box>
+          </Paper>
+        </Grid>
+      ))}
     </Grid>
   );
 };
