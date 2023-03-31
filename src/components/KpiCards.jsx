1ec512656f82c934e0927be0220475e5419bbadd
Yusuf <baliyusuf675@gmail.com>
2023-03-31 16:01:55 +0300

    charts

--- src/components/KpiCards.jsx
+++ src/components/KpiCards.jsx
@@ -1,61 +1,75 @@
-import { Avatar, Grid, Paper, Typography } from "@mui/material";
+import { Avatar, Box, Grid, Paper, Typography } from "@mui/material";
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import PaymentsIcon from "@mui/icons-material/Payments";
-import { Box } from "@mui/system";
+import { amber, deepPurple, pink } from "@mui/material/colors";
+import { useSelector } from "react-redux";
 
 const KpiCards = () => {
-  const iconSize = {
-    ".MuiSvgIcon-root": { fontSize: "2" },
-  };
+  const { sales, purchases } = useSelector((state) => state.stock);
 
   const totalSales = sales
-    .map((item) => item.price_total)
+    .map((item) => Number(item.price_total))
     .reduce((acc, val) => acc + val, 0);
+
+  const totalPurchases = purchases
+    .map((item) => Number(item.price_total))
+    .reduce((acc, val) => acc + val, 0);
+
+  const totalProfit = totalSales - totalPurchases;
   const data = [
     {
       id: 1,
       title: "sales",
-      value: "$40000",
-      icon: <MonetizationOnIcon />,
+      value: `$${totalSales}`,
+      icon: <MonetizationOnIcon sx={{ fontSize: "2.3rem" }} />,
       color: deepPurple[600],
       bgColor: deepPurple[100],
     },
     {
       id: 2,
       title: "profit",
-      value: "$30000",
-      icon: <ShoppingCartIcon />,
+      value: `$${totalProfit}`,
+      icon: <ShoppingCartIcon sx={{ fontSize: "2.3rem" }} />,
       color: pink[600],
       bgColor: pink[100],
     },
     {
       id: 3,
       title: "purchases",
-      value: "$10000",
-      icon: <ShoppingCartIcon sx={iconSize} />,
+      value: `$${totalPurchases}`,
+      icon: <PaymentsIcon sx={{ fontSize: "2.3rem" }} />,
       color: amber[600],
       bgColor: amber[100],
     },
   ];
 
-  console.log(totalSales);
   return (
     <Grid container justifyContent={"center"} spacing={3}>
       {data.map((item) => (
-        <Grid item sm={12} md={6} lg={4}>
-          <Paper sx={{ p: 2 }}>
-            <Box sx={{ display: "flex", alignItems: "center", gap: 3 }}>
+        <Grid
+          item
+          key={item.id}
+          xs={12}
+          sm={10}
+          md={6}
+          lg={4}
+          sx={{ minWidth: "250px" }}
+        >
+          <Paper sx={{ p: 2 }} elevation={10}>
+            <Box sx={{ display: "flex", alignItems: "center", gap: 5 }}>
               <Avatar
                 sx={{
                   backgroundColor: item.bgColor,
                   color: item.color,
-                  width: 60,
-                  height,
+                  width: 70,
+                  height: 70,
+                  ml: 4,
                 }}
               >
                 {item.icon}
               </Avatar>
+
               <Box>
                 <Typography variant="button">{item.title}</Typography>
                 <Typography variant="h4">{item.value}</Typography>
